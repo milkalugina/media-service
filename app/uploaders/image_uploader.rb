b6bad89968c96ff71ea217b9ccba85632bbebd37
Lily Akhmetova <55274620+lilyakhmetova@users.noreply.github.com>
2020-10-20 13:05:54 +0300

    Доверстала интерфейс админки

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   process resize_to_fill: [440, 238]
-
+  radius = 20
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
