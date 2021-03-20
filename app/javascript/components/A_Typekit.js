0a667b05d90d01daa45c292b515f3b6de5aebdad
Mila Kalugina <milkalugin@gmail.com>
2021-03-20 23:01:55 +0300

    Подключила React к Rails, попробовала создать тестовый компонент

---
+++ app/javascript/components/A_Typekit.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class A_Typekit extends React.Component {
+  render () {
+    return (
+        <h1>
+          A_textblock
+        </h1>
+    );
+  }
+}
+
+export default A_Typekit
