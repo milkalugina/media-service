d1b72be6aa91733ce658d54e9aeb35fbaf9b4139
Mila Kalugina <milkalugin@gmail.com>
2021-03-23 14:18:17 +0300

    Подключила шрифты

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # не всегда, но при лайке появлялась ошибка InvalidAuthenticityToken, поэтому добавляю это
-  skip_before_action :verify_authenticity_token
 
     protected
 
@@ -10,4 +8,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 			devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
+
+    # не всегда, но при лайке появлялась ошибка InvalidAuthenticityToken, поэтому добавляю это
+    skip_before_action :verify_authenticity_token
 end
