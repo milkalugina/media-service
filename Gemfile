33a7c3c4ec96bc1f3ce692060debdbdcfbcda3e8
Lily Akhmetova <55274620+lilyakhmetova@users.noreply.github.com>
2021-06-11 00:28:20 +0300

    Реализация тегов OpenGraph и SEO

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "react-rails", "~> 2.6"
+
+gem 'meta-tags'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'dobryedela.ru',
+      title: 'Добрые дела',
+      reverse: true,
+      separator: '—',
+      description: 'Медиа-сервис для волонтеров и НКО',
+      keywords: 'волонтерство, НКО, организация, волонтер',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        # { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'dobryedela.ru',
+        title: 'Добрые дела',
+        description: 'Медиа-сервис для волонтеров и НКО',
+        type: 'website',
+        url: request.original_url,
+        # image: image_url('login-page.png')
+      }
+    }
+  end
 end
