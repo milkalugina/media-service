2adc8e64405e00ec83784232d3046791c593b38f
Lily Akhmetova <55274620+lilyakhmetova@users.noreply.github.com>
2021-06-12 20:38:18 +0300

    Сделала разное отображение новостей, создала посты

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,9 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  process resize_to_fill: [440, 238]
-  radius = 20
-  
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
