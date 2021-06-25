a3a70e045d7e07fbb7e3d7f3bbecfcffba15db33
Mila Kalugina <55274798+milkalugina@users.noreply.github.com>
2021-06-26 02:13:23 +0300

    drop delete

--- db/migrate/20210322183116_drop_table_favorite.rb
+++
@@ -1,5 +0,0 @@
-class DropTableFavorite < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :favorites
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.destroy_all
-Post.destroy_all
+# Event.destroy_all
+# Post.destroy_all
 
-puts "Destroyed everything you touch"
+# puts "Destroyed everything you touch"
+
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 # posts = [
 # {
