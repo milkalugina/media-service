eeeda238d26780fc0a7fad6bdba2f96749700244
Mila Kalugina <55274798+milkalugina@users.noreply.github.com>
2021-06-26 08:43:42 +0300

    Испрвила файл helper, изменила валидацию

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,16 @@ module ApplicationHelper
       canonical: request.original_url,
       noindex: !Rails.env.production?,
       icon: [
-        # { href: image_url('favicon.ico') },
-        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      # { href: image_url('favicon.ico') },
+      # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       ],
       og: {
-        site_name: 'dobryedela.ru',
-        title: 'Добрые дела',
-        description: 'Медиа-сервис для волонтеров и НКО',
-        type: 'website',
-        url: request.original_url,
-        # image: image_url('login-page.png')
+      site_name: 'dobryedela.ru',
+      title: 'Добрые дела — медиа-сервис для волонтеров и НКО',
+      description: '«Добрые дела» — медиа-сервис, который помогает волонтерам и активистам найти для себя мероприятие или проект, в котором хотелось бы поучаствовать',
+      type: 'website',
+      url: request.original_url,
+      # image: image_url('login-page.png')
       }
     }
   end
